877423d8e977e1b16b9444e28d59948ede1207e5
SiddheshLende <siddheshlende77@gmail.com>
2026-08-01 09:50:22 +0530

    second commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Page setup
 st.set_page_config(
-    page_title="Portfolio | Cloud & Python Engineer!",
+    page_title="Portfolio | Cloud & Python Engineer....",
     page_icon="⚡",
     layout="wide",
 )
